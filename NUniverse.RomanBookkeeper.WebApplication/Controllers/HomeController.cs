08b08ca64f30b9dec9063a8099382d5bbd064a20
Bogdan Marian <bogdan.marian77@gmail.com>
2013-06-05 23:26:15 +0300

    Initial support for adding numbers.

---
+++ NUniverse.RomanBookkeeper.WebApplication/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+
+namespace NUniverse.RomanBookkeeper.WebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ NUniverse.RomanBookkeeper.WebApplication/Controllers/NumbersController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+
+namespace NUniverse.RomanBookkeeper.WebApplication.Controllers
+{
+    public class NumbersController : Controller
+    {
+        //
+        // GET: /Numbers/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
