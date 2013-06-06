203317afa9534d19c6a78934956e24246d2961c6
Bogdan Marian <bogdan.marian77@gmail.com>
2013-06-06 23:46:42 +0300

    Added flesh to the web application.

--- NUniverse.RomanBookkeeper.WebApplication/Controllers/HomeController.cs
+++ NUniverse.RomanBookkeeper.WebApplication/Controllers/HomeController.cs
@@ -4,11 +4,22 @@ namespace NUniverse.RomanBookkeeper.WebApplication.Controllers
 {
     public class HomeController : Controller
     {
-        //
-        // GET: /Home/
-
         public ActionResult Index()
         {
+            ViewBag.Message = "";
+            return View();
+        }
+
+        public ActionResult Summing()
+        {
+            //throw new Exception("BOOOM!", new Exception("Root Cause ;)"));
+            ViewBag.Message = "Perform summing of two Roman or Arabic numbers";
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "This web application has been developed by Bogdan Marian";
             return View();
         }
     }
--- NUniverse.RomanBookkeeper.WebApplication/Global.asax.cs
+++ NUniverse.RomanBookkeeper.WebApplication/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System.Web.Mvc;
+﻿using System.Web.Http;
+using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 
@@ -6,36 +7,16 @@ namespace NUniverse.RomanBookkeeper.WebApplication
 {
     // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
     // visit http://go.microsoft.com/?LinkId=9394801
+
     public class MvcApplication : System.Web.HttpApplication
     {
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
-            RegisterGlobalFilters(GlobalFilters.Filters);
-            RegisterRoutes(RouteTable.Routes);
-            RegisterBundles(BundleTable.Bundles);
-        }
-
-        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
-        {
-            filters.Add(new HandleErrorAttribute());
-        }
-
-        public static void RegisterRoutes(RouteCollection routes)
-        {
-            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
-            routes.MapRoute(
-                            name: "Default",
-                            url: "{controller}/{action}/{id}",
-                            defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-                    );
-        }
-
-        public static void RegisterBundles(BundleCollection bundles)
-        {
-            bundles.Add(new ScriptBundle("~/jquery")
-                                .Include("~/Scripts/jquery-1.4.4.js"));
+            WebApiConfig.Register(GlobalConfiguration.Configuration);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
