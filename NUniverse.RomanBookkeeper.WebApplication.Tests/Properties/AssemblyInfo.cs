7343b6009379e8924128f9ac6c1cda18604bb84d
Bogdan Marian <bogdan.marian77@gmail.com>
2013-06-08 20:44:50 +0300

    Added test project for the web application.

---
+++ NUniverse.RomanBookkeeper.WebApplication.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("NUniverse.RomanBookkeeper.WebApplication.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NUniverse.RomanBookkeeper.WebApplication.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
