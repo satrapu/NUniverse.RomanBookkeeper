053b03c0db8305c3a687af554e873cc21239e709
Bogdan Marian <bogdan.marian77@gmail.com>
2013-06-11 21:30:40 +0300

    Added more SummingController tests.

--- NUniverse.RomanBookkeeper.WebApplication.Tests/Controllers/SummingControllerTests.cs
+++ NUniverse.RomanBookkeeper.WebApplication.Tests/Controllers/SummingControllerTests.cs
@@ -1,6 +1,7 @@
 ﻿using System.Web.Mvc;
 using NUnit.Framework;
 using NUniverse.RomanBookkeeper.WebApplication.Controllers;
+using NUniverse.RomanBookkeeper.WebApplication.Models;
 
 namespace NUniverse.RomanBookkeeper.WebApplication.Tests.Controllers
 {
@@ -13,6 +14,51 @@ namespace NUniverse.RomanBookkeeper.WebApplication.Tests.Controllers
             SummingController summingController = new SummingController();
             ViewResult viewResult = (ViewResult) summingController.Index();
             Assert.AreEqual("Summing", viewResult.ViewBag.Title);
+        }
+
+        [Test]
+        public void DoSumming_UsingMissingLeftOperand_DisplaysError()
+        {
+            SummingModel summingModel = new SummingModel();
+            SummingController summingController = new SummingController();
+            summingController.DoSumming(summingModel);
+            Assert.IsTrue(summingController.ModelState.ContainsKey("LeftOperand"));
+        }
+
+        [Test]
+        public void DoSumming_UsingInvalidRomanNumberAsLeftOperand_DisplaysError()
+        {
+            SummingModel summingModel = new SummingModel {LeftOperand = "IIII", RightOperand = "MD"};
+            SummingController summingController = new SummingController();
+            summingController.DoSumming(summingModel);
+            Assert.IsTrue(summingController.ModelState.ContainsKey("LeftOperand"));
+        }
+
+        [Test]
+        public void DoSumming_UsingMissingRightOperand_DisplaysError()
+        {
+            SummingModel summingModel = new SummingModel {LeftOperand = "leftoperand"};
+            SummingController summingController = new SummingController();
+            summingController.DoSumming(summingModel);
+            Assert.IsTrue(summingController.ModelState.ContainsKey("RightOperand"));
+        }
+
+        [Test]
+        public void DoSumming_UsingInvalidRomanNumberAsRightOperand_DisplaysError()
+        {
+            SummingModel summingModel = new SummingModel {LeftOperand = "MD", RightOperand = "IIII"};
+            SummingController summingController = new SummingController();
+            summingController.DoSumming(summingModel);
+            Assert.IsTrue(summingController.ModelState.ContainsKey("RightOperand"));
+        }
+
+        [Test]
+        public void DoSumming_UsingRomanNumbersAsLeftAndRightOperand_DisplaysSummingResult()
+        {
+            SummingModel summingModel = new SummingModel {LeftOperand = "MD", RightOperand = "LX"};
+            SummingController summingController = new SummingController();
+            ViewResult viewResult = (ViewResult) summingController.DoSumming(summingModel);
+            Assert.AreEqual("MDLX", viewResult.ViewBag.SummingResult);
         }
     }
 }
